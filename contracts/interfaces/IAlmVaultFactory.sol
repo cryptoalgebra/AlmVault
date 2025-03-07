7456d4bd4cbf238fe7c48b57df9679880dde2cad
fourlen <oborin.2013@mail.ru>
2025-03-07 13:15:50 +0300

    +factory

--- contracts/interfaces/IAlmVaultFactory.sol
+++ contracts/interfaces/IAlmVaultFactory.sol
@@ -3,59 +3,58 @@
 pragma solidity 0.7.6;
 
 interface IAlmVaultFactory {
-    event FeeRecipient(address indexed sender, address feeRecipient);
 
-    event AmmFee(address indexed sender, uint256 ammFee);
+    event FeeRecipient(
+        address indexed sender, 
+        address feeRecipient);
 
-    event BaseFee(address indexed sender, uint256 baseFee);
+    event AmmFee(
+        address indexed sender, 
+        uint256 ammFee);
 
-    event BaseFeeSplit(address indexed sender, uint256 baseFeeSplit);
+    event BaseFee(
+        address indexed sender, 
+        uint256 baseFee);
 
-    event DeployAlmVaultFactory(address indexed sender, address uniswapV3Factory);
+    event BaseFeeSplit(
+        address indexed sender, 
+        uint256 baseFeeSplit);
+    
+    event DeployAlmVaultFactory(
+        address indexed sender, 
+        address algebraFactory);
 
     event AlmVaultCreated(
-        address indexed sender,
-        address almVault,
+        address indexed sender, 
+        address almVault, 
         address tokenA,
         bool allowTokenA,
         address tokenB,
         bool allowTokenB,
-        uint24 fee,
-        uint256 count
-    );
+        uint256 count);    
 
-    function uniswapV3Factory() external view returns (address);
-
-    function feeRecipient() external view returns (address);
-
+    function algebraFactory() external view returns(address);
+    function feeRecipient() external view returns(address);
     function ammFee() external view returns(uint256);
-
-    function baseFee() external view returns (uint256);
-
-    function baseFeeSplit() external view returns (uint256);
-
+    function baseFee() external view returns(uint256);
+    function baseFeeSplit() external view returns(uint256);
+    
     function setFeeRecipient(address _feeRecipient) external;
-
     function setAmmFee(uint256 _ammFee) external;
-
     function setBaseFee(uint256 _baseFee) external;
-
     function setBaseFeeSplit(uint256 _baseFeeSplit) external;
 
     function createAlmVault(
         address tokenA,
         bool allowTokenA,
         address tokenB,
-        bool allowTokenB,
-        uint24 fee
+        bool allowTokenB
     ) external returns (address almVault);
 
     function genKey(
-        address deployer,
-        address token0,
-        address token1,
-        uint24 fee,
-        bool allowToken0,
-        bool allowToken1
-    ) external pure returns (bytes32 key);
+        address deployer, 
+        address token0, 
+        address token1, 
+        bool allowToken0, 
+        bool allowToken1) external pure returns(bytes32 key);
 }
---
+++ contracts/libraries/AlmVaultDeployer.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.7.6;
+
+import {AlmVault} from '../AlmVault.sol';
+import {IAlgebraFactory} from '@cryptoalgebra/v1-core/contracts/interfaces/IAlgebraFactory.sol';
+
+library AlmVaultDeployer {
+
+    function createAlmVault(
+        address pool, 
+        address token0,
+        bool allowToken0,
+        address token1,
+        bool allowToken1,
+        uint32 twapPeriod,
+        uint256 vaultIndex
+    ) public returns(address almVault) {
+
+        almVault = address(
+            new AlmVault{salt: keccak256(abi.encodePacked(
+                msg.sender, 
+                token0, 
+                allowToken0, 
+                token1, 
+                allowToken1)
+            )}
+                (pool, 
+                allowToken0, 
+                allowToken1, 
+                msg.sender, 
+                twapPeriod,
+                vaultIndex)
+        );
+    }
+}
