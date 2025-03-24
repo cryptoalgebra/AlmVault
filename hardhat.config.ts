d92364a9d23e5845ce4a6ece4fe13084feaf0579
debych <zh.eliseev@yandex.ru>
2025-03-24 16:26:50 +0300

    update project files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
+import "@nomiclabs/hardhat-etherscan";
+import { config as dotenvConfig } from "dotenv";
+import "hardhat-deploy";
+import type { HardhatUserConfig } from "hardhat/config";
+import type { NetworkUserConfig } from "hardhat/types";
+import { resolve } from "path";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   typechain: {
-    outDir: "typechain",
+    outDir: "types",
   }
 };
 
