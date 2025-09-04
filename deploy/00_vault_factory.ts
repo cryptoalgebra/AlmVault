66b2944672fe4e7d5a46b5703b41295f2360c6b3
IliaAzhel <aje-ilia2013@yandex.by>
2025-09-04 16:56:44 +0300

    add fees setter to deploy script

--- deploy/00_vault_factory.ts
+++ deploy/00_vault_factory.ts
@@ -3,10 +3,14 @@ import { UV3Math, AlgebraVaultFactory } from "../types";
 const hre = require("hardhat");
 
 async function main() {
-    const algebraFactory = "0x904Af47469B13b341B41c552c952370b76B69DFA"
+    const algebraFactory = "0x41ba59415eC75AC4242dd157F2a7A282F1e75652"
     const pluginDeployer = "0x0000000000000000000000000000000000000000" // zero address for base pools
-    const nftManager = "0xDE4E488b8F835E8c7Bc9d2d307fff804625aCA76"
-    const wrapNative = "0x4200000000000000000000000000000000000006"
+    const nftManager = "0x578D8A2D07B60b12993559f1DDF90EB2af3eA496"
+    const wrapNative = "0x5555555555555555555555555555555555555555"
+
+    const feeRecipient = "0x0000000000000000000000000000000000000000" // algebra fee address
+    const ammFee = "100000000000000000" // 10%(100% is 10**18), partner's fees
+    const baseFee = "100000000000000000" // 10% 
 
     const uV3MathFactory = await hre.ethers.getContractFactory("UV3Math");
     const uV3Math = (await uV3MathFactory.deploy()) as UV3Math;
@@ -29,6 +33,14 @@ async function main() {
     await AlgebraVaultFactory.deployed()
 
     console.log("AlgebraVaultFactory to:", AlgebraVaultFactory.address);
+
+    // Set fee configuration
+    console.log("Setting fee recipient to:", feeRecipient);
+    await AlgebraVaultFactory.setFeeRecipient(feeRecipient);
+
+    console.log("Setting AMM fees...");
+    await AlgebraVaultFactory.setAmmFee(ammFee);
+    await AlgebraVaultFactory.setBaseFee(baseFee);
 
     const AlgebraVaultDepositGuardFactory = await hre.ethers.getContractFactory("AlgebraVaultDepositGuard");
     const AlgebraVaultDepositGuard = await AlgebraVaultDepositGuardFactory.deploy(AlgebraVaultFactory.address, wrapNative) as AlgebraVaultFactory;
